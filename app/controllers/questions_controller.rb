27894ef8e44f72a243725abc49abff006080e4e7
veyrr2 <gorokhov_d@iuca.kg>
2025-05-15 16:32:00 +0600

    5 lesson

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,25 @@
-class QuestionsController < ApplicationController
+class QuestionsController < ApplicationController    
     def create
         # создаём вопрос, передавая параметры через params
-        Question.create body: params[:question][:body], user_id: params[:question][:user_id]
+        @question = Question.create body: params[:question][:body], user_id: params[:question][:user_id]
 
-        # переместить пользователя на главный сайт. Слеш - это и есть главная страница.
-        redirect_to '/'
+        # переместить пользователя на вопрос.
+        redirect_to (@question)
     end
 
+    # обновить вопрос от edit
     def update 
         # сначала находим
         @question = Question.find(params[:id])
 
         # обновляем
-        @question = Question.update(
+        @question.update(
             body: params[:question][:body],
             user_id: params[:question][:user_id]
         )
+
+        # перенаправляем
+        redirect_to question_path(@question)
     end
 
     def destroy
@@ -26,12 +30,24 @@ class QuestionsController < ApplicationController
         @question = Question.destroy
     end
 
+    # показать один вопрос
     def show
         # сначала находим
         @question = Question.find(params[:id])
     end 
 
+    # список вопросов
     def index 
         @questions = Question.all
+    end
+
+    # создать вопрос в форме
+    def new 
+        @question = Question.new
+    end
+
+    # редактировать вопрос
+    def edit 
+        @question = Question.find(params[:id]) 
     end
 end
